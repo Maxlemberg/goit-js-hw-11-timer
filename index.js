a49149aa58b82b1521bf0b9a18bed13f67552abf
Maksym <maksymuhf@gmail.com>
2021-02-04 19:21:37 +0200

    +timer

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ class CountdownTimer {
 
 const timer = new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date(2021, 01, 04, 17),
+  targetDate: new Date(2021, 01, 04, 20),
 });
 
 timer.addTimer();
